d3630252ed1585ae47b3172fb896210295f4d3c5
Marijana Igracki <marijana.makili@gmail.com>
2023-01-30 12:32:08 -0500

    Cucumber-Junit

---
+++ src/test/java/com/cybertek/step_definitions/GoogleStepDefinitions.java
@@ -0,0 +1,23 @@
+package com.cybertek.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class GoogleStepDefinitions {
+
+
+    @Given("User is on the google search page")
+    public void user_is_on_the_google_search_page() {
+        System.out.println("User is on the Google search page!");
+    }
+
+    @Then("User should see title is Google")
+    public void user_should_see_title_is_google() {
+        System.out.println("User sees the title is Google!");
+    }
+
+
+
+
+
+}
---
+++ src/test/resources/features/Search.feature
@@ -0,0 +1,8 @@
+Feature: Search
+  Agile story:As I User,I should be able to search when I am on Google search page
+
+  Scenario: Google default title verification
+    Given User is on the google search page
+    Then User should see title is Google
+
+
